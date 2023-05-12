cabf7ab94a6d4efbc8176aa22b28c5e87d625805
Crowdbotics <team@crowdbotics.com>
2023-05-12 14:23:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "polished-sound-40650",
+  url: "https://polished-sound-40650.botics.co",
+  api: "https://polished-sound-40650.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
